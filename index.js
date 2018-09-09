a4978e06103792231cf6238e75eae38858dfe808
Marissa So <sotom591@gmail.com>
2018-09-09 02:09:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var recipes = {};
 var obj = {prop: 1};
 function updateObjectWithKeyAndValue(object, key, value)
 {
- Object.assign({}, obj, {[key]: value});
- var obj = {prop2: 2};
+ Object.assign({}, object, {[key]: value});
+ return object 
 }
